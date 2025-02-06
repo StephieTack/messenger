e40364f71186edc3208199e8cb88679790f2fd6e
Stephanie Tack <stephierathjen@gmx.de>
2025-02-06 13:14:49 +0100

    integrate backend and websocket.service

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import * as WebSocket from 'ws';
+// const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+  });
+
+  ws.send('Welcome to the WebSocket server!');
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
